66ce325e70dad11875a8f898e11ad3492a1edfef
Maqsood Alam <maqsood2420@gmail.com>
2023-10-09 14:39:44 +0530

    Be endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-
-export const socket = io('http://localhost:5000')
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketchbook-server-3d5s.onrender.com' : 'http://localhost:5000'
+export const socket = io(URL);
